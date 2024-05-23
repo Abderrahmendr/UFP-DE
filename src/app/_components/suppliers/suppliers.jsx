5b77b43146eaf5a66516b5f825431aa1b7372519
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2024-05-23 13:50:29 +0100

    change

--- src/app/_components/suppliers/suppliers.jsx
+++ src/app/_components/suppliers/suppliers.jsx
@@ -1,120 +1,174 @@
  import React from 'react'
 import Marquee from 'react-fast-marquee' 
+import Image from 'next/image'
  function suppliers() {
-   return (
-    <div>
-      <div class=" flex justify-center items-center  p-2   ">
-			<img 
-			className="opacity-20"
-			src="/UFP_logo.png" 
-			alt="" />
-			<p className="flex justify-center items-center -ml-56 mt-12  text-3xl pb-16 font-extrabold
-			
-			
-			"
-			
-			> Unsere Markenprodukt </p>
-				</div>
-       <div className='w-full min-h-full flex   overflow-y-hidden justify-center items-center bg-teal-800 text-white'>
-     
-     <div className=" w-full ">
-      
-      <Marquee className='overflow-y-hidden pl-20 gap-x-20 space-x-[10px]'>
-<a href="#" className=''>
-  <img
-  src="/panasonic.png"
-  />
-</a>
+   return ( 
 
-<a href="#" className=''>
-  <img
-  src="/hp.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/printmate.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/printronix.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/quantum.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/samsung.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/ricoh.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/sharp.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/toshiba.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/olivetti.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/oki.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/lexmark.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/kodak.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/canon-1.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/brother-1.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/dlink.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/apple.png"
-  />
-</a>
-<a href="#" className=''>
-  <img
-  src="/dell.png"
-  />
-</a>
+<div >
+  <div className="flex justify-center items-center w-full min-h-full  ">
+    <Image 
+      className="opacity-20"
+      src="/UFP_logo.png" 
+      alt="UFP Logo" 
+      width={150} 
+      height={150} 
+    />
+    <p className="flex justify-center items-center -ml-56 mt-12 text-3xl pb-16 font-extrabold">
+      Unsere Markenprodukt
+    </p>
+  </div>
+  <div className="w-full min-h-full flex overflow-y-hidden justify-center items-center bg-teal-800 text-white">
+    <div className="w-full">
+      <Marquee className="overflow-y-hidden pl-20 gap-x-20 space-x-[10px]">
+        <a href="#">
+          <Image
+            src="/panasonic.png"
+            alt="Panasonic"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/hp.png"
+            alt="HP"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/printmate.png"
+            alt="Printmate"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/printronix.png"
+            alt="Printronix"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/quantum.png"
+            alt="Quantum"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/samsung.png"
+            alt="Samsung"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/ricoh.png"
+            alt="Ricoh"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/sharp.png"
+            alt="Sharp"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/toshiba.png"
+            alt="Toshiba"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/olivetti.png"
+            alt="Olivetti"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/oki.png"
+            alt="Oki"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/lexmark.png"
+            alt="Lexmark"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/kodak.png"
+            alt="Kodak"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/canon-1.png"
+            alt="Canon"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/brother-1.png"
+            alt="Brother"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/dlink.png"
+            alt="D-Link"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/apple.png"
+            alt="Apple"
+            width={100} 
+            height={100} 
+          />
+        </a>
+        <a href="#">
+          <Image
+            src="/dell.png"
+            alt="Dell"
+            width={100} 
+            height={100} 
+          />
+        </a>
       </Marquee>
-     </div>
-     </div>
     </div>
+  </div>
+</div>
+
     
    )
  }
