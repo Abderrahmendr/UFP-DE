03a6508602fd9c5ea101470db39c246854e7f999
Abderrahmendr <dorbaneabderrahmen@gmail.com>
2024-05-24 08:50:25 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Dots from '@/app/_components/dots'
 import Social_media from "@/app/_components/social_media"
 import ScrollTopButton from "@/app/_components/scrollTopButton"
-import Products from "@/app/_components/Products/products"
-import Suppliers from "@/app/_components/suppliers/suppliers"
-import  Engagement  from "@/app/_components/engagement/engagement";
+import Products from "@/app/Products/products"
+import Suppliers from "@/app/suppliers/suppliers"
+import  Engagement  from "@/app/engagement/engagement";
    export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pt-7  ">
